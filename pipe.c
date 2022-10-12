314717c231c0c8821c36474cec859245d9b93c2b
Brian Taylor <btaylor9@ucla.edu>
2022-10-11 23:59:04 -0700

    v3

--- pipe.c
+++ pipe.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++) {
-		printf("%s", argv[i]);
-	}
+	int fds[2];
+	pipe(fds);
+	// int ret = fork();
+	// if (ret == 0) {
+
+	// }
+	dup2(fd[1], STDOUT_FILENO);
+	system(argv[1]);
+	dup2(fd[0], STDIN_FILENO);
+	system(argv[2]);
 	return 0;
 }
