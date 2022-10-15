2abde1d5349d405d958b3816cbdde410b75c15b7
Brian Taylor <btaylor9@ucla.edu>
2022-10-15 15:06:47 -0700

    v6

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Pipe Up
 
-One sentence description
+This program replicates the pipe operator found in shells and allows users to pipe together the inputs and outputs of executables.
 
 ## Building
 
-Explain briefly how to build your program
+The program is built by entering the lab1 directory, which must contain pipe.c and the Makefile, and executing the command "make".
 
 ## Running
 
-Show an example run of your program, using at least two additional arguments, and what to expect
+An example command would be "./pipe ls cat wc". The expected output would be the output from the command "ls | cat | wc", which should be three numbers representing the number of lines, words, and characters in the output of "ls".
 
 ## Cleaning up
 
-Explain briefly how to clean up all binary files
+The lab1 directory can be cleaned up by entering the lab1 directory and executing the command "make clean", which will remove all binary files.
--- pipe.c
+++ pipe.c
@@ -2,32 +2,47 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+
 int main(int argc, char *argv[])
 {
     int saved_stdout = dup(1);
     int fds[2];
 	int i;
+	if (argc == 1) { // no arguments
+		errno = EINVAL;
+		return -1;
+	}
 	for (i = 1; i < argc - 1; i++) {
     	pipe(fds);
     	int ret = fork();
+
+		// child process
     	if (ret == 0) {
-        	dup2(fds[1], 1);
+        	dup2(fds[1], 1); // directs output to write-end of pipe
 			if (execlp(argv[i], argv[i], (char *)NULL) == -1) {
 				perror(argv[i]);
 				return -1;
 			}
         	exit(0);
     	}
+
+		// parent process
     	else if (ret > 0) {
 			int status;
 			waitpid(ret, &status, 0);
         	dup2(fds[0], 0);
 			close(fds[1]);
+			close(fds[0]);
     	}
+
+		// fork failed
 		else {
+			return -1;
 		}
+
 	}
-    dup2(saved_stdout, 1);
+    dup2(saved_stdout, 1); // redirect output to standard output
 	if (execlp(argv[argc-1], argv[argc-1], (char *)NULL) == -1) {
 		perror(argv[argc-1]);
 		return -1;
